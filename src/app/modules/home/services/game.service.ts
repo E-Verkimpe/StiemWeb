1305d97730ef1ee687113c6b128a021b38ba3096
Eliano Verkimpe <Eliano.verkimpe@gmail.com>
2022-11-28 14:53:12 +0100

    get gamelist from API

--- src/app/modules/home/services/game.service.ts
+++ src/app/modules/home/services/game.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { GameDto } from '../Models/game-dto';
+import { map, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
+  private url = "Game";
 
-  constructor() { }
+  constructor(private client: HttpClient) { }
+
+  public GetGames(): Observable<GameDto[]>{
+    return this.client.get<GameDto[]>(`${environment.apiUrl}/${this.url}`)
+  }
 }
